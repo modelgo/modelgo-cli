b8df90b3cbc439aa272d7fc569a759a6db8932f9
renjiegod <297910962@qq.com>
2026-05-25 14:42:32 +0800

    feat(version): add version package with build-time ldflags injection

---
+++ internal/version/version.go
@@ -0,0 +1,5 @@
+// Package version exposes the modelgo-cli version string.
+// Set at build time via -ldflags "-X github.com/modelgo/modelgo-cli/internal/version.Version=v1.2.3".
+package version
+
+var Version = "dev"
